66d274a4de1d8f814f06e4eae36d08841daf88de
Даниил Меркулов <daniilmerkulov@MacBook-Air-Daniil.local>
2025-04-03 22:13:02 +0300

    Чистка postgres_connection.py

---
+++ src/postgres_connection.py
@@ -0,0 +1,37 @@
+import os
+
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+# Загрузка переменных окружения из файла .env
+load_dotenv()
+
+# Получение параметров из .env
+HOST = os.getenv("HOST")
+DB_PORT = os.getenv("PG_PORT")
+PG_DB = os.getenv("PG_DB")
+PG_LOGIN = os.getenv("PG_LOGIN")
+PG_PASS = os.getenv("PG_PASS")
+
+# Формирование строки подключения через SQLAlchemy
+DATABASE_URL = f"postgresql+psycopg2://{PG_LOGIN}:{PG_PASS}@{HOST}:{DB_PORT}/{PG_DB}?client_encoding=utf8"
+
+# Создание SQLAlchemy Engine
+engine = create_engine(DATABASE_URL)
+
+query = """
+    SELECT table_name 
+    FROM information_schema.tables 
+    WHERE table_schema = 'dwh' AND table_type = 'BASE TABLE'
+"""
+
+# Пример: Чтение данных из таблицы в Pandas DataFrame
+def connection(table: str) -> pd.DataFrame:
+    df = pd.read_sql(f"SELECT * FROM {table}", engine)
+    df = df.applymap(lambda x: x.decode('latin1').encode('utf-8') if isinstance(x, bytes) else x)
+    return df
+
+def all_tables() -> pd.DataFrame:
+    df_tables = pd.read_sql(query, engine)
+    return df_tables
