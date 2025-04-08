02fc73ce21e6e98927fac57dc0607a81b7de4ee3
Даниил Меркулов <daniilmerkulov@MacBook-Air-Daniil.local>
2025-04-08 20:54:38 +0300

    Сделал EDA

--- src/postgres_connection.py
+++ src/postgres_connection.py
@@ -1,12 +1,13 @@
 import os
-
 import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', 10)
 # Загрузка переменных окружения из файла .env
 load_dotenv()
-
 # Получение параметров из .env
 HOST = os.getenv("HOST")
 DB_PORT = os.getenv("PG_PORT")
@@ -15,7 +16,7 @@ PG_LOGIN = os.getenv("PG_LOGIN")
 PG_PASS = os.getenv("PG_PASS")
 
 # Формирование строки подключения через SQLAlchemy
-DATABASE_URL = f"postgresql+psycopg2://{PG_LOGIN}:{PG_PASS}@{HOST}:{DB_PORT}/{PG_DB}?client_encoding=utf8"
+DATABASE_URL = f"postgresql+psycopg2://{PG_LOGIN}:{PG_PASS}@{HOST}:{DB_PORT}/{PG_DB}"
 
 # Создание SQLAlchemy Engine
 engine = create_engine(DATABASE_URL)
@@ -26,12 +27,12 @@ query = """
     WHERE table_schema = 'dwh' AND table_type = 'BASE TABLE'
 """
 
-# Пример: Чтение данных из таблицы в Pandas DataFrame
+#Пример: Чтение данных из таблицы в Pandas DataFrame
 def connection(table: str) -> pd.DataFrame:
     df = pd.read_sql(f"SELECT * FROM {table}", engine)
-    df = df.applymap(lambda x: x.decode('latin1').encode('utf-8') if isinstance(x, bytes) else x)
     return df
 
 def all_tables() -> pd.DataFrame:
     df_tables = pd.read_sql(query, engine)
     return df_tables
+
